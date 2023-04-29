6f93c9accf68287677078e784002a20762a215e0
Imran Ahmed <imran.a6444@gmail.com>
2023-04-29 12:48:16 +0500

    my first assignment react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <h1>Mubarak Ho App chal rahi ha</h1>
+     <h1>Aur Html pr Css b apply ho rahi ha</h1>
+    </>
+  );
+}
+
+export default App;
